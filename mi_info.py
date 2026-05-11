eea47a3529c1cf2a9aeeb9d7e8bb40b3ad3a3e36
Carla Fioritto <fiorittocarla@live.com.ar>
2026-05-11 20:31:08 -0300

    Agregar script con información personal

---
+++ mi_info.py
@@ -0,0 +1,7 @@
+# mi_info.py - Información personal
+nombre = "Carla"
+edad = 27 # Cambiá por tu edad
+materia = "Software Factory"
+print(f"Hola, soy {nombre}")
+print(f"Tengo {edad} años")
+print(f"Estoy cursando {materia}")
